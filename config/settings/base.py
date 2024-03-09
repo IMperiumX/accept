cf2b698c5b2f062845f1b7c9a854546850e7ff1a
IMperiumX <yusufadell.dev@gmail.com>
2024-03-09 22:28:52 +0200

    Add API views and serializers for films and related models

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,8 +10,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = BASE_DIR / "accept"
 env = environ.Env()
 
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
-if READ_DOT_ENV_FILE:
+if READ_DOT_ENV_FILE := env.bool("DJANGO_READ_DOT_ENV_FILE", default=False):
     # OS environment variables take precedence over variables from .env
     env.read_env(str(BASE_DIR / ".env"))
 
